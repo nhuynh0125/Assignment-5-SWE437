678e6d57f8385cb7cfbe2778e320826d338d14d5
QuangVo0601 <44596914+QuangVo0601@users.noreply.github.com>
2019-03-05 12:06:55 -0500

    Working UI

--- src/quizretakes/InstructorUI.java
+++ src/quizretakes/InstructorUI.java
@@ -8,25 +8,34 @@ import java.util.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+
+
 public class InstructorUI {
 	
-	private static String quizzesFileName = "quiz-orig-swe437.xml";
-    private static String retakesFileName = "quiz-retakes-swe437.xml";
-	public static String dataLocation = "src/"; /* CLI */
+	private static String dataLocation = "/Users/ryanvo1/Documents/workspace/swe437-Assignment4/"; /* CLI */
+	private static String quizzesFileName = dataLocation + "quiz-orig-swe437.xml";
+    private static String retakesFileName = dataLocation + "quiz-retakes-swe437.xml";
 	private static Scanner sc;
 
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, Exception {
 		// TODO Auto-generated method stub
 		
 		   System.out.println("");
 		   System.out.println("");
 		   System.out.println("******************************************************************************");
-		   System.out.println("GMU quiz retake scheduler for class SWE437 (Instructor Use)");
+		   System.out.println("GMU quiz retake scheduler for class SWE 437 (Instructor Use)");
 		   System.out.println("******************************************************************************");
 		   System.out.println("");
 		   System.out.println("");
@@ -63,7 +72,7 @@ public class InstructorUI {
 	 * @throws IOException 
 	 * @throws SAXException 
 	 */
-	public static void appendQuizNodeToXML() throws ParserConfigurationException, SAXException, IOException{
+	public static boolean appendQuizNodeToXML() throws ParserConfigurationException, SAXException, IOException, Exception{
 				
 		System.out.println("Let's create a new quiz");
 		
@@ -72,17 +81,63 @@ public class InstructorUI {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		Document document = builder.parse(new File(quizzesFileName));
+				
+		Element root = document.getDocumentElement();
+		Element rootElement = document.getDocumentElement();
 		
-		// Get all the nodes
-		NodeList nodeList = document.getDocumentElement().getChildNodes();
-		
-		
-		
-	}
+		Collection<quizBean> quizzes = new ArrayList<quizBean>();
+	    quizzes.add(newQuiz);
+	    
+	    for(quizBean i : quizzes){
+	    	  Element quiz = document.createElement("quiz");
+	          rootElement.appendChild(quiz);
+
+	          Element id = document.createElement("id");
+	          id.appendChild(document.createTextNode(Integer.toString(i.getID())));
+	          quiz.appendChild(id);
+
+	          Element dateGiven = document.createElement("dateGiven");
+	          quiz.appendChild(dateGiven);
+	          
+	          Element month = document.createElement("month");
+	          month.appendChild(document.createTextNode(Integer.toString(i.getMonthNum())));
+	          dateGiven.appendChild(month);
+	          
+	          Element day = document.createElement("day");
+	          day.appendChild(document.createTextNode(Integer.toString(i.getDate().getDayOfMonth())));
+	          dateGiven.appendChild(day);
+	          
+	          Element hour = document.createElement("hour");
+	          hour.appendChild(document.createTextNode(i.timeAsString().substring(0, 2)));
+	          dateGiven.appendChild(hour);
+	          
+	          Element minute = document.createElement("minute");
+	          minute.appendChild(document.createTextNode(i.timeAsString().substring(3)));
+	          dateGiven.appendChild(minute);
+
+	          root.appendChild(quiz);
+	    }
+	    
+	    DOMSource source = new DOMSource(document);
+
+	    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	    Transformer transformer = transformerFactory.newTransformer();
+	    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+	    StreamResult result = new StreamResult(quizzesFileName);
+	    transformer.transform(source, result);
+	    
+	    System.out.println("The quiz was added to the system successfully");
+	    
+	    return true;
+	  }
 	
+	/**
+	 * Create a new quiz by entering the information
+	 * @return the new created quiz
+	 */
 	private static quizBean createNewQuiz(){
 		
-		//public quizBean(int quizID, int month, int day, int hour, int minute) 
 		System.out.println("Enter a quiz ID: ");
 		int quizID = sc.nextInt();
 		
@@ -95,23 +150,111 @@ public class InstructorUI {
 		System.out.println("Enter the hour: ");
 		int hour = sc.nextInt();
 		
-		System.out.println("Enter the minute minute: ");
+		System.out.println("Enter the minute: ");
 		int minute = sc.nextInt();
-		
-		//checking if data is validated, check later
-//		LocalDate today  = LocalDate.now();
-//		
-//		if(today.)
 		
 		quizBean newQuiz = new quizBean(quizID, month, day, hour, minute );
 		
-		return newQuiz;
+	    return newQuiz;
 	}
 	
 	/**
 	 * Append a new retake to quiz-retakes-swe437.xml
+	 * @throws ParserConfigurationException 
+	 * @throws IOException 
+	 * @throws SAXException 
 	 */
-	public static void appendRetakeNodeToXML(){
+	public static void appendRetakeNodeToXML() throws ParserConfigurationException, SAXException, IOException, Exception{
+		
+		System.out.println("Let's create a new retake");
+		
+		retakeBean newRetake = createNewRetake();
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.parse(new File(retakesFileName));
+				
+		Element root = document.getDocumentElement();
+		Element rootElement = document.getDocumentElement();
+		
+		Collection<retakeBean> retakes = new ArrayList<retakeBean>();
+	    retakes.add(newRetake);
+	    
+	    for(retakeBean i : retakes){
+	    	  Element retake = document.createElement("retake");
+	          rootElement.appendChild(retake);
+
+	          Element id = document.createElement("id");
+	          id.appendChild(document.createTextNode(Integer.toString(i.getID())));
+	          retake.appendChild(id);
+	          
+	          Element location = document.createElement("location");
+	          location.appendChild(document.createTextNode(i.getLocation()));
+	          retake.appendChild(location);
+
+	          Element dateGiven = document.createElement("dateGiven");
+	          retake.appendChild(dateGiven);
+	          
+	          Element month = document.createElement("month");
+	          month.appendChild(document.createTextNode(Integer.toString(i.getMonthNum())));
+	          dateGiven.appendChild(month);
+	          
+	          Element day = document.createElement("day");
+	          day.appendChild(document.createTextNode(Integer.toString(i.getDate().getDayOfMonth())));
+	          dateGiven.appendChild(day);
+	          
+	          Element hour = document.createElement("hour");
+	          hour.appendChild(document.createTextNode(i.timeAsString().substring(0, 2)));
+	          dateGiven.appendChild(hour);
+	          
+	          Element minute = document.createElement("minute");
+	          minute.appendChild(document.createTextNode(i.timeAsString().substring(3)));
+	          dateGiven.appendChild(minute);
+
+	          root.appendChild(retake);
+	    }
+	    
+	    DOMSource source = new DOMSource(document);
+
+	    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	    Transformer transformer = transformerFactory.newTransformer();
+	    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+	    StreamResult result = new StreamResult(retakesFileName);
+	    transformer.transform(source, result);
+	    
+	    System.out.println("The retake was added to the system successfully");		
+		
+	}
+	
+	/**
+	 * Create a new retake by entering the information
+	 * @return the new created retake
+	 */
+	private static retakeBean createNewRetake(){
+		
+		System.out.println("Enter a retake ID: ");
+		int quizID = sc.nextInt();
+		
+		System.out.println("Enter the location: ");
+		String location = sc.next();
+		System.out.print("");
+		
+		System.out.println("Enter the month: ");
+		int month = sc.nextInt();
+		
+		System.out.println("Enter the day: ");
+		int day = sc.nextInt();
+		
+		System.out.println("Enter the hour: ");
+		int hour = sc.nextInt();
+		
+		System.out.println("Enter the minute: ");
+		int minute = sc.nextInt();
+				
+		retakeBean newRetake = new retakeBean(quizID, location, month, day, hour, minute );
+		
+		return newRetake;
 		
 	}
 
